478fccfa689011dec39e0c639f07f43f312fb6a1
stansful <stansful@gmail.com>
2022-09-13 19:29:10 +0300

    Move some necessary variables outside

--- src/index.js
+++ src/index.js
@@ -9,29 +9,30 @@ const AuthService = require('./services/auth');
 const UploadService = require('./services/upload');
 
 const credentials = {
-	PHONE_NUMBER: process.env.PHONE_NUMBER, // '+79991113322',
-	API_ID: process.env.API_ID, // '7777777',
-	API_HASH: process.env.API_HASH, //'dadsd67sgdds67gag76',
+	PHONE_NUMBER: process.env.PHONE_NUMBER,
+	API_ID: process.env.API_ID,
+	API_HASH: process.env.API_HASH,
 };
-const savePath = path.join(__dirname, 'storage', 'secretSession.json');
+
+const myChannelId = '1793809121';
+const videoName = 'The.Lord.of.the.Rings.The.Rings.of.Power.S01E01.720p.ColdFilm.mp4';
+const mimeType = 'video/mp4';
+const videoPath = path.join(__dirname, 'videos', videoName);
+const sessionSavePath = path.join(__dirname, 'storage', 'secretSession.json');
 
 const start = async () => {
 	try {
-		const mtproto = new MtprotoService(credentials.API_ID, credentials.API_HASH, savePath);
+		const mtproto = new MtprotoService(credentials.API_ID, credentials.API_HASH, sessionSavePath);
 		const auth = new AuthService(mtproto, credentials.PHONE_NUMBER);
 		const user = await auth.getUser();
 
 		if (!user) {
-			// send confirm code to telegram app
+			console.log('\nPlease enter code from telegram app:\n');
 			await auth.signIn(auth);
 		}
 
 		const { chats } = await mtproto.call('messages.getAllChats', { except_ids: [] });
-		// console.log(chats, 'AllChats:') // find required chat by id or name... whatever
-		const { id, access_hash } = chats.filter(chat => chat._ === 'channel').find(chat => chat.id === '1793809121');
-
-		const videoName = 'The.Lord.of.the.Rings.The.Rings.of.Power.S01E01.720p.ColdFilm.mp4';
-		const videoPath = path.join(__dirname, 'videos', videoName);
+		const { id, access_hash } = chats.filter(chat => chat._ === 'channel').find(chat => chat.id === myChannelId);
 
 		const fileBuffer = fs.readFileSync(videoPath);
 		const fileId = random(64);
@@ -51,7 +52,7 @@ const start = async () => {
 		const media = {
 			_: 'inputMediaUploadedDocument',
 			file: inputFile,
-			mime_type: 'video/mp4',
+			mime_type: mimeType,
 			attributes: [
 				{
 					_: 'documentAttributeVideo',
@@ -69,9 +70,10 @@ const start = async () => {
 			peer: sendingToChannel,
 			random_id: random(64),
 		});
-		console.log(result);
+
+		console.log(result, '\nSuccessfully uploaded!');
 	} catch (e) {
-		console.log(e, '\nERROR\n');
+		console.log(e, '\nERROR Happend!\n');
 	}
 };
 start();
